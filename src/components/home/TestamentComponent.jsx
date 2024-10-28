d81b7017652b918b9ee729240cf5a75393d22737
Diego Bravo Arias <digobravo@outlook.com>
2024-10-28 16:04:23 -0500

    Added format for images

--- src/components/home/TestamentComponent.jsx
+++ src/components/home/TestamentComponent.jsx
@@ -3,15 +3,23 @@ import React from "react";
 import { Link } from "react-router-dom";
 const TestamentComponent = ({ characters }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2 mx-2">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 mx-2">
       {characters.map((character) => (
-        <div
+        <Link
+          to={`/details/${character.name}`}
           key={character.name}
           className="bg-white shadow-md p-4 border border-gray-200"
         >
-          <Link to={`/details/${character.name}`} className="text-xl font-semibold mb-2">{character.name}</Link>
-          <p className="text-gray-600">{character.text}</p>
-        </div>
+          <div>
+            <img src={character.image} className="rounded-full max-94 y-94"/>
+          </div>
+          <div className="text-xl font-semibold font-poppins mb-2">
+            {character.name}
+            <p className="text-gray-600 font-normal font-monserrat">
+              {character.text}
+            </p>
+          </div>
+        </Link>
       ))}
     </div>
   );
